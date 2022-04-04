ea87fe5fe6dad4a5cb36848219b31f686bc61833
lijiaman <961892352@qq.com>
2022-04-04 20:28:44 +0800

    psutil study ok

---
+++ psutilStudy.py
@@ -0,0 +1,23 @@
+import psutil
+
+# cpu
+print(psutil.cpu_count())
+print(psutil.cpu_count(logical=False))
+
+# memory
+print(psutil.virtual_memory())
+print(psutil.virtual_memory().percent)
+
+# disk
+print(psutil.disk_partitions())
+print(psutil.disk_usage("c:/"))
+
+# processlist
+print(psutil.pids())
+
+print(psutil.Process(664))
+print(psutil.Process(664).exe())
+print(osutil.Process(664).connections())
+
+# user info
+print(psutil.disk_io_counters(perdisk=True))
